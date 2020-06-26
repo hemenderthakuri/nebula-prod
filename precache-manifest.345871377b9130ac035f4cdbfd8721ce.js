8a1d6c6c5ae8ec2a367f9ef77f15c6b2939587be
Hemender Thakuri <Hemender.Thakuri@harbingergroup.com>
2020-06-27 03:09:07 +0530

    updated react app

---
+++ precache-manifest.345871377b9130ac035f4cdbfd8721ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19c49e2de7e46bc396279a2480f5c382",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2d5ded9cb7d4af78717e",
+    "url": "/static/css/main.223a76b0.chunk.css"
+  },
+  {
+    "revision": "4424c036c2e0953d88be",
+    "url": "/static/js/2.b3c32563.chunk.js"
+  },
+  {
+    "revision": "a42b5950f1c797e7f18ad9ee1971ac1a",
+    "url": "/static/js/2.b3c32563.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d5ded9cb7d4af78717e",
+    "url": "/static/js/main.a00285bb.chunk.js"
+  },
+  {
+    "revision": "931889094ef5210074c3",
+    "url": "/static/js/runtime-main.48311f9e.js"
+  }
+]);
